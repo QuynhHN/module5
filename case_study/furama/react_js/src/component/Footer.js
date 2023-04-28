d05bbf1c3b4d12edb876d86e1c10d95d618ff4cb
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2023-04-28 16:33:22 +0700

    update

---
+++ case_study/furama/react_js/src/component/Footer.js
@@ -0,0 +1,102 @@
+import {Component} from "react";
+
+export class  Footer extends Component{
+    render() {
+        return(
+            <>
+                <footer className="text-center text-lg-start bg-light text-muted">
+                    <section className="" style={{backgroundColor: "#addccf",height: "auto"}}>
+
+                        <div className="container text-center text-md-start">
+                            <div className="row mt-2">
+                                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4"
+                                     style={{backgroundColor: "#f3f3f3",width: "320px"}}>
+                                    <h6 className="text-uppercase fw-bold mb-4"
+                                        style={{marginTop: "50px", color: "#cbbe73", fontSize: "25px",textAlign: "center"}}>
+                                        <i className="fas fa-gem me-3"></i>Hướng dẫn di chuyển
+                                    </h6>
+                                    <p style="font-size: 13px">
+                                        Khu nghỉ dưỡng Furama là cơ sở hàng đầu để khám phá một trong những điểm đến hấp
+                                        dẫn nhất Châu
+                                        Á.
+                                        Chỉ cách Đà Nẵng một quãng lái xe ngắn là bốn Di sản Văn hóa Thế giới được
+                                        UNESCO công nhận
+                                    </p>
+                                    <button
+                                        style={{marginLeft: "20%", backgroundColor: "#046056", borderColor: "#046056",color: "white", height: "40px"}}>
+                                        Xem trên bản đồ
+                                    </button>
+                                    <p style={{color: "#cbbe73", marginTop: "15px"}}>Địa điểm</p>
+                                    <table className="table" style={{fontSize: "13px"}}>
+                                        <tr>
+                                            <td>1.Cố đô Huế</td>
+                                            <td>2 tiếng</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2.Phố cổ Hội An</td>
+                                            <td>30 phút</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3.Thánh địa Mỹ Sơn</td>
+                                            <td>90 phút</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4.Động Phong Nha</td>
+                                            <td>3 tiếng</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                    <ul style={{marginTop: "50px", marginLeft: "-70px"}}>
+                                        <li style={{textAlign: "left"}}>Giá công bố</li>
+                                        <li style={{textAlign: "left"}}>LifeStyle blog</li>
+                                        <li style={{textAlign: "left"}}>Tuyển dụng</li>
+                                        <li style={{textAlign: "left"}}>Liên hệ</li>
+                                    </ul>
+                                    <img width="300%" style={{marginTop: "170px", marginLeft: "-70px"}}
+                                         src="https://furamavietnam.com/wp-content/uploads/2023/03/Ariyana-Tourism-Complex-02-500x131.png"/>
+                                </div>
+                                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4"
+                                     style={{marginTop: "50px"}}>
+                                    <div className="block-23 mb-3">
+                                        <h6 className="text-uppercase fw-bold mb-4" style="color: #cbbe73">Liên hệ</h6>
+                                        <ul>
+                                            <li><span className="icon icon-map-marker"
+                                                      style={{color:"2a2a29"}}></span><span className="text"
+                                                                                          style={{color: "#2a2a29",marginLeft: "10px"}}>103 – 105 Đường Võ Nguyên Giáp, Phường Khuê Mỹ, Quận Ngũ hành Sơn, Tp. Đà Nẵng, Việt Nam</span>
+                                            </li>
+                                            <li><a><span className="icon icon-phone" style={{color:"2a2a29"}}></span><span
+                                                className="text"
+                                                style={{color: "#2a2a29",marginLeft: "10px"}}>84-236-3847 333/888</span></a>
+                                            </li>
+                                            <li><a><span className="icon icon-envelope"
+                                                         style={{color:"2a2a29"}}></span><span
+                                                className="text"
+                                                style={{color: "#2a2a29",marginLeft: "20px"}}>reservation@furamavietnam.com</span></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <hr/>
+                                        <img style={{width: "32px",height: "32px",float: "left"}}
+                                             src="https://furamavietnam.com/wp-content/uploads/2019/06/incon-Facbook-1.png"
+                                             alt=""
+                                             width="32" height="32"/>
+                                            <img style={{width: "40px",height: "32px",float: "left"}}
+                                                 src="https://furamavietnam.com/wp-content/uploads/2019/06/social-ins.png"
+                                                 alt="" width="40"
+                                                 height="32"/>
+                                                <img style={{width: "40px",height: "32px",float: "left"}}
+                                                     src="https://furamavietnam.com/wp-content/uploads/2021/10/social-youtube.png"
+                                                     alt="" width="40"
+                                                     height="32"/>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </footer>
+                </>
+        )
+    }
+
+}
