525d045e4740df277a263273f5f673a86f137e0c
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2023-04-28 19:37:10 +0700

    update

--- case_study/furama/react_js/src/component/Header.js
+++ case_study/furama/react_js/src/component/Header.js
@@ -5,25 +5,14 @@ export class Header extends Component{
         return (
             <>
                 <header>
-                    <nav
-                        className="header-fixed"
-                        style={{ position: "fixed", zIndex: 2, left: 0, right: 0 }}
-                    >
-                        <div
-                            style={{ backgroundColor: "#fffafa" }}
-                            className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom"
-                        >
-                            <a
-                                href="#"
-                                className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
-                                style={{ marginRight: "-220px" }}
-                            >
-                                <img
-                                    width="40px"
-                                    style={{ marginLeft: 100 }}
-                                    src="https://furamavietnam.com/wp-content/uploads/2018/08/logo.png"
-                                    alt=""
-                                />
+                    <nav className="header-fixed" style={{position: "fixed", zIndex: 2, left: 0, right: 0}}>
+                        <div style={{backgroundColor: "#fffafa"}}
+                             className="d-flex align-items-center justify-content-center py-3 border-bottom">
+                            <a href="#"
+                               className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+                               style={{marginRight: -220 + "px"}}>
+                                <img width="40px" style={{marginLeft: 100}}
+                                     src="https://furamavietnam.com/wp-content/uploads/2018/08/logo.png" alt=""/>
                             </a>
                             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                                 <li>
@@ -70,13 +59,10 @@ export class Header extends Component{
                                     <a
                                         className="nav-link dropdown-toggle fs-5 px-4 fw-bold text-dark text-hover"
                                         href="#"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
+                                        type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Quản lí
                                     </a>
-                                    <ul className="dropdown-menu" style={{ backgroundColor: "#fffafa" }}>
+                                    <ul className="dropdown-menu" style={{backgroundColor: "#fffafa"}}>
                                         <li>
                                             <a className="dropdown-item" href="#">
                                                 Dịch vụ
@@ -109,7 +95,7 @@ export class Header extends Component{
                         </div>
                     </nav>
                 </header>
-            </>
+                </>
     )
     }
 
