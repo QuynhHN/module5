8f248a5ed4926aac0a27e46f505fd506b5dc634f
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2023-05-09 16:49:20 +0700

    update

---
+++ ss7_redux/user_management/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {UserManagement} from "./component/UserManagement";
+import React from "react";
+
+function App() {
+    return (
+        <>
+            <UserManagement/>
+        </>
+    );
+}
+
+export default App;
